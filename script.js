9311601e3efab3a2bf5296638bd03d2b62a6b8c4
Ana Paula Vieira da Silva <134345302+Ana-Paula-7@users.noreply.github.com>
2025-07-29 21:24:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let participantes = [];
 
-function adicionarParticipante() {
+function adicionarParticipante() 
+ alert("Função adicionar chamada!");
+{
   const nomeInput = document.getElementById("nome");
   const nome = nomeInput.value.trim();
 
